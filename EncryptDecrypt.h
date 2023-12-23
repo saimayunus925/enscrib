99a3ea465af2a508bd348714f1b74aee48fdc713
Saima Yunus <saimayunus925@gmail.com>
2023-12-23 02:10:36 -0800

    cleaning up and revising the EncryptDecrypt class

--- EncryptDecrypt.h
+++ EncryptDecrypt.h
@@ -17,13 +17,19 @@ using std::vector;
 
 class EncryptDecrypt {
     private:
-        long __KEY; // key we'll use to encrypt/decrypt stuff
+        long __KEY; // symmetric key we'll use to encrypt/decrypt stuff
         string plain_text; // plaintext to be encrypted
         long cipher_text; // encrypted resulting ciphertext to be decrypted
     public:
-        long RSA_encrypt(string plain_text); // encrypts (with RSA) "plain_text" and returns the int ciphertext
-        string RSA_decrypt(long cipher_text); // decrypts (with RSA) "cipher_text" and returns the translated string plaintext
+        EncryptDecrypt(); // default constructor
+        EncryptDecrypt(string PLAINTEXT); // parametrized constructor (parameter for plaintext string)
+        void RSA_encrypt(); // encrypts (with RSA) "plain_text" and stores the result in "ciphertext"
+        void RSA_decrypt(); // decrypts (with RSA) "cipher_text" and stores the result in "plaintext"
         // TODO: make encrypt/decrypt functions for AES, DES, and other cryptography algorithms
+        void AES_encrypt(); // encrypts (with AES) "plain_text" and stores the result in "ciphertext"
+        void AES_decrypt(); // decrypts (with AES) "cipher_text" and stores the result in "plaintext"
+        void DES_encrypt(); // encrypts (with DES) "plain_text" and stores the result in "ciphertext"
+        void DES_decrypt(); // decrypts (with DES) "cipher_text" and stores the result in "plaintext"
 };
 
 
