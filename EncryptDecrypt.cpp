e5afbae31fea5dc0af428909b7c394a78d80897a
Saima Yunus <saimayunus925@gmail.com>
2023-12-23 01:57:10 -0800

    wrote basic header file for encrypt/decrypt class

--- EncryptDecrypt.cpp
+++ EncryptDecrypt.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "EncryptDecrypt.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using std::cin, std::cout, std::endl, std::ifstream, std::ofstream;
+using std::string;
+using std::vector;
+
+// TODO: write the encrypt/decrypt algorithms for RSA, AES, and DES. Make sure they encrypt plaintext files and decrypt resulting ciphertext correctly.
--- EncryptDecrypt.h
+++ EncryptDecrypt.h
@@ -5,9 +5,25 @@
 #ifndef FILE_ENCRYPTION_PROJECT_ENCRYPTDECRYPT_H
 #define FILE_ENCRYPTION_PROJECT_ENCRYPTDECRYPT_H
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using std::cin, std::cout, std::endl, std::ifstream, std::ofstream;
+using std::string;
+using std::vector;
 
 class EncryptDecrypt {
-
+    private:
+        long __KEY; // key we'll use to encrypt/decrypt stuff
+        string plain_text; // plaintext to be encrypted
+        long cipher_text; // encrypted resulting ciphertext to be decrypted
+    public:
+        long RSA_encrypt(string plain_text); // encrypts (with RSA) "plain_text" and returns the int ciphertext
+        string RSA_decrypt(long cipher_text); // decrypts (with RSA) "cipher_text" and returns the translated string plaintext
+        // TODO: make encrypt/decrypt functions for AES, DES, and other cryptography algorithms
 };
 
 
