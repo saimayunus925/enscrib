52ae8aa4b49c295958ad9c1d0739985a75ad84ba
Saima Yunus <saimayunus925@gmail.com>
2023-12-23 01:53:37 -0800

    started class for file IO methods

--- File_IO.cpp
+++ File_IO.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "File_IO.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using std::cin, std::cout, std::endl, std::ifstream, std::ofstream;
+using std::string;
+using std::vector;
+
+File_IO::File_IO() {
+    // initializing both file names to blank strings
+    this->inputFile = "";
+    this->outputFile = "";
+}
+
+File_IO::File_IO(std::string INPUT_FILENAME) {
+    // initializing input file name to passed-in name and output file name to blank string
+    this->inputFile = INPUT_FILENAME;
+    this->outputFile = "";
+}
+
+File_IO::File_IO(std::string INPUT_FILENAME, std::string OUTPUT_FILENAME) {
+    // initializing both file names to the passed-in names
+    this->inputFile = INPUT_FILENAME;
+    this->outputFile = OUTPUT_FILENAME;
+}
+
+void File_IO::read_input_lines() {
+    // step 1: open the input file
+    ifstream input; // the file stream object
+    input.open(this->inputFile); // opening the file
+    // step 2: make sure the file is open
+    bool OPEN = input.is_open(); // this returns true if the file opened correctly
+    if (!OPEN) {
+        perror("ERROR"); // this prints "ERROR" and then the error message for clarity
+        exit(1); // exit if file didn't open
+    }
+    // step 3: read each line of the file into the 'file_content' vector
+    while (!input.eof()) {
+        // as long as we're not at the end of the file, we'll read each line of it
+        string newLine = ""; // the variable to store each line of the file
+        getline(input, newLine); // the big step: reading in the current line from our file into 'newLine' str
+        this->file_content.push_back(newLine) // add the 'newLine' var (with our current file line's content) into 'file_content' vector
+    }
+    input.close(); // closing file now that we're done with it
+}
--- File_IO.h
+++ File_IO.h
@@ -5,9 +5,27 @@
 #ifndef FILE_ENCRYPTION_PROJECT_FILE_IO_H
 #define FILE_ENCRYPTION_PROJECT_FILE_IO_H
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using std::cin, std::cout, std::endl, std::ifstream, std::ofstream;
+using std::string;
+using std::vector;
+
 
 class File_IO {
-
+    private:
+        string inputFile; // name of input file with data to be encrypted/decrypted
+        string outputFile; // name of output file where resulting encrypted/decrypted data is written
+        vector <string> file_content; // vector where we'll read each line of the file to be encrypted/decrypted
+    public:
+        File_IO(); // default constructor
+        File_IO(string INPUT_FILENAME); // parametrized constructor with only the input file name
+        File_IO(string INPUT_FILENAME, string OUTPUT_FILENAME); // parametrized constructor with both file names (input file and output file)
+        void read_input_lines(); // reads each line of the input file into 'file_content' vector
+        void write_output_lines(); // writes each line of 'file_content' vector into output file
 };
 
 
